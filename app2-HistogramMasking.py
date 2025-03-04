71fc4529ec9574f84e29138a003990549d06cb4e
kenj <kenjieramos22@gmail.com>
2025-03-04 10:41:08 +0800

    eh

--- app2-HistogramMasking.py
+++ app2-HistogramMasking.py
@@ -10,23 +10,22 @@ app = Flask(__name__)
 
 model = tf.keras.models.load_model("KmeansHistogramYellow.keras")  
 
-def extract_yellow_histogram_features(image, bins=10):
-    image = np.array(image.convert("RGB"))
-
+def extract_color_features(image, bins=10, lightness_scale=0.8):
+    image = np.array(image.convert("RGB")) 
     lab_image = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
-    L, A, B = lab_image[:, :, 0], lab_image[:, :, 1], lab_image[:, :, 2]
-
-    L_valid, A_valid, B_valid = L.flatten(), A.flatten(), B.flatten()
-
-    if L_valid.size == 0:
-        return np.zeros(bins * 3)
-
-    L_hist = np.histogram(L_valid / 255.0, bins=bins, range=(0, 1), density=True)[0]
-    A_hist = np.histogram((A_valid + 128) / 255.0, bins=bins, range=(0, 1), density=True)[0]
-    B_hist = np.histogram((B_valid + 128) / 255.0, bins=bins, range=(0, 1), density=True)[0]
-
-    feature_vector = np.concatenate([L_hist, A_hist, B_hist])
-
+    
+    lightness, a_channel, b_channel = lab_image[:, :, 0], lab_image[:, :, 1], lab_image[:, :, 2]
+    
+    lightness = (lightness / 255.0)
+    a_channel = (a_channel + 128) / 255.0 
+    b_channel = (b_channel + 128) / 255.0
+    
+    lightness_hist = np.histogram(lightness, bins=bins, range=(0, 1), density=True)[0]
+    a_hist = np.histogram(a_channel, bins=bins, range=(0, 1), density=True)[0]
+    b_hist = np.histogram(b_channel, bins=bins, range=(0, 1), density=True)[0]
+    
+    feature_vector = np.concatenate([lightness_hist, a_hist, b_hist])
+    
     return feature_vector
 
 
@@ -67,7 +66,7 @@ def predict():
     image_file = request.files['image']
     image = Image.open(io.BytesIO(image_file.read()))
     bins = 10  
-    features = extract_yellow_histogram_features(image, bins).reshape(1, -1)
+    features = extract_color_features(image, bins).reshape(1, -1)
     expected_features = bins * 3
 
     if features.shape[1] != expected_features:
